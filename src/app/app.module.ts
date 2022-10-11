5f887c304627d9d8117011b607919076afe282b3
alindo <andreslindo20@gmail.com>
2022-10-11 00:31:29 -0400

    building out components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import {} from @
+
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MenuItemComponent } from './components/menu-item/menu-item.component';
+import { MenuGridComponent } from './components/menu-grid/menu-grid.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MenuItemComponent,
+    MenuGridComponent
   ],
   imports: [
     BrowserModule
